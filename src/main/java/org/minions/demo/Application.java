dee88ec2607d8c9a4f4fb00c6b2d2b0d65a94b74
Salaboy <salaboy@gmail.com>
2018-05-09 10:25:17 +0200

    adding actuators

--- src/main/java/org/minions/demo/Application.java
+++ src/main/java/org/minions/demo/Application.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application implements CommandLineRunner {
 
     private static final Log log = LogFactory.getLog(Application.class);
